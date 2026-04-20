151a778989c66956c6cac2acead84ee5fc1c5e59
David Preti <Doudi@mac-mini.home>
2026-04-20 10:34:40 +0200

    fix: add habit snapshot storage to app target

--- Streakmap/WidgetSupport/WidgetStorage.swift
+++ Streakmap/WidgetSupport/WidgetStorage.swift
@@ -2,6 +2,7 @@ import Foundation
 
 enum WidgetStorage {
     static let globalSnapshotKey = "streakmap.widget.globalSnapshot"
+    static let habitSnapshotKey = "streakmap.widget.habitSnapshot"
 
     private static var defaults: UserDefaults {
         UserDefaults(suiteName: WidgetConfig.appGroupID) ?? .standard
@@ -19,5 +20,19 @@ enum WidgetStorage {
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         return try? decoder.decode(GlobalHeatmapWidgetSnapshot.self, from: data)
+    }
+
+    static func saveHabitSnapshot(_ snapshot: HabitHeatmapWidgetSnapshot) {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        guard let data = try? encoder.encode(snapshot) else { return }
+        defaults.set(data, forKey: habitSnapshotKey)
+    }
+
+    static func loadHabitSnapshot() -> HabitHeatmapWidgetSnapshot? {
+        guard let data = defaults.data(forKey: habitSnapshotKey) else { return nil }
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return try? decoder.decode(HabitHeatmapWidgetSnapshot.self, from: data)
     }
 }

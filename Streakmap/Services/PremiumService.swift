7ae422a56e0a60c70a812ce2beffa37d2b71c90e
David <david.preti@naquada.ch>
2026-04-21 17:41:13 +0200

    feat: Remove deprecated PremiumService

--- Streakmap/Services/PremiumService.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-final class PremiumService: ObservableObject {
-    @Published var isPremiumUnlocked: Bool = false
-
-    func unlockPremium() {
-        isPremiumUnlocked = true
-    }
-}
